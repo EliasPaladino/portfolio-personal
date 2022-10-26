ca39f294cb280e9cf91f88595d65a7f23f3b41de
EliasPaladino <eliaspaladino@outlook.com>
2022-10-26 00:00:18 -0300

    Add Grid and Typography

--- src/components/CenteredText.js
+++ src/components/CenteredText.js
@@ -1,1 +1,15 @@
-export function CenteredText() {}
+import { Grid, Typography } from '@mui/material';
+
+export function CenteredText() {
+  <Grid container direction='column' justify='space-around'>
+    <Grid item xs={8}>
+      <Typography />
+    </Grid>
+    <Grid item xs={4}>
+      <Typography />
+    </Grid>
+    <Grid item xs={4}>
+      <Typography />
+    </Grid>
+  </Grid>;
+}
