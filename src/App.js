8f6eaac94fe30ad53b2eb04ad1f269c9d988dd91
EliasPaladino <eliaspaladino@outlook.com>
2022-10-27 11:41:10 -0300

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { CenteredText } from './components/CenteredText';
 import { NavBar } from './components/NavBar';
 
 function App() {
   return (
     <div className='App'>
       <NavBar />
+      <CenteredText />
     </div>
   );
 }
--- src/components/CenteredText.js
+++ src/components/CenteredText.js
@@ -1,15 +1,30 @@
 import { Grid, Typography } from '@mui/material';
+import './CenteredText.scss';
 
 export function CenteredText() {
-  <Grid container direction='column' justify='space-around'>
-    <Grid item xs={8}>
-      <Typography />
+  return (
+    <Grid
+      className='centeredText'
+      container
+      direction='column'
+      justify='space-around'
+      alignItems='center'
+    >
+      <Grid item xs={8}>
+        <Typography variant='body1' align='center' gutterBottom='true'>
+          Hola
+        </Typography>
+      </Grid>
+      <Grid item xs={8}>
+        <Typography variant='body1' align='center' gutterBottom='true'>
+          <b>asdasddsasda</b>
+        </Typography>
+      </Grid>
+      <Grid item xs={8}>
+        <Typography variant='body1' align='center' gutterBottom='true'>
+          asdsdasdaasd
+        </Typography>
+      </Grid>
     </Grid>
-    <Grid item xs={4}>
-      <Typography />
-    </Grid>
-    <Grid item xs={4}>
-      <Typography />
-    </Grid>
-  </Grid>;
+  );
 }
