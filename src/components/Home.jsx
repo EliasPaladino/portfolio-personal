bf89a7132c45a23ef83336827804f825dc7705dd
EliasPaladino <eliaspaladino@outlook.com>
2022-11-02 00:42:18 -0300

    Add btn conoceme

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import meImage from '../assets/me.webp';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const Home = () => {
   return (
@@ -7,7 +8,7 @@ const Home = () => {
       name='home'
       className='bg-gradient-to-b from-black via-black to-gray-800 h-screen w-full flex justify-center items-center'
     >
-      <div className='max-w-screen-lg mx-auto flex flex-col h-full justify-center items-center px-8 md:flex-row md:justify-between h-80'>
+      <div className='max-w-screen-lg gap-6 mx-auto flex flex-col h-full justify-center items-center px-8 md:flex-row md:justify-between'>
         <div className='flex flex-col justify-center md:px-6'>
           <h2 className='text-white text-4xl sm:text-7xl font-bold'>
             Hola, soy un desarrollador Full Stack
@@ -16,8 +17,16 @@ const Home = () => {
             Me apasiona el desarrollo web, actualmente utilizo tecnologias como
             React, Java, Tailwind y MySql.
           </p>
-          3
+          <div>
+            <button className='group flex justify-between gap-3 items-center text-white px-2 py-2 my-2 rounded-lg bg-gradient-to-br '>
+              <span className=''>Conoceme</span>
+              <span className='group-hover:rotate-90 duration-300'>
+                <ArrowForwardIcon />
+              </span>
+            </button>
+          </div>
         </div>
+
         <div className='max-w-full'>
           <img
             src={meImage}
